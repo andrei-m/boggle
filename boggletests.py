3a8303d83ed32147330a6b67a8f6acd163d4ddd8
Andrei Mackenzie <andrei@ccs.neu.edu>
2012-06-07 00:27:52 -0400

    additional tests covering 'isWord' for non-leaf trie nodes and exclusion of words containing q[^u]

--- boggletests.py
+++ boggletests.py
@@ -17,10 +17,28 @@ class TrieParseTests(unittest.TestCase):
         currentNode = currentNode.children[ord('e') - 97]
         self.assertTrue(currentNode.isWord)
 
+    def testIsWord(self):
+        """Test the 'isWord' flag for a non-leaf node"""
+        currentNode = self.root.children[ord('a') - 97]
+        self.assertFalse(currentNode.isWord)
+        currentNode = currentNode.children[ord('d') - 97]
+        self.assertFalse(currentNode.isWord)
+        currentNode = currentNode.children[ord('d') - 97]
+        self.assertTrue(currentNode.isWord)
+        currentNode = currentNode.children[ord('e') - 97]
+        self.assertFalse(currentNode.isWord)
+        currentNode = currentNode.children[ord('r') - 97]
+        self.assertTrue(currentNode.isWord)
+
+
     def testExcludeShortWord(self):
         """Assert that words shorter than 3 chars are excluded"""
         # 'pi' is excluded; there is no other word beginning with 'p'
         self.assertTrue(self.root.children[ord('p') - 97] is None)
+
+    def testExcludeQnotU(self):
+        """Assert that the word with a q lacking a following u is excluded"""
+        self.assertTrue(self.root.children[ord('s') - 97] is None)
 
 if __name__ == '__main__':
     unittest.main()
