c4431889c1acde6305b99c7e41b805ea6de14352
Andrei Mackenzie <andrei@ccs.neu.edu>
2012-06-10 14:07:14 -0400

    Added a few tests for word finding. The 'adda' test exposes an issue with character reuse

--- boggletests.py
+++ boggletests.py
@@ -40,6 +40,30 @@ class TrieParseTests(unittest.TestCase):
         """Assert that the word with a q lacking a following u is excluded"""
         self.assertTrue(self.root.children[ord('s') - 97] is None)
 
+class BoggleTests(unittest.TestCase):
+    """Tests for finding words given a grid and word trie"""
+    
+    def setUp(self):
+        self.dict = boggle.MakeTrie('testdict.txt')
+    
+    def testQuad(self):
+        grid = ['qu', 'ad']
+        words = boggle.BoggleWords(grid, self.dict)
+        self.assertEquals(set(['qad']), words)
+
+    def testAdder(self):
+        grid = ['adq', 'sdf', 'erp']
+        words = boggle.BoggleWords(grid, self.dict)
+        self.assertEquals(set(['add', 'adder']), words)
+
+    def testCharacterReuse(self):
+        """Characters that have previously been used as part of a word cannot
+           be reused in the same word"""
+        grid = ['ad', 'dp']
+        words = boggle.BoggleWords(grid, self.dict)
+        # 'adda' is excluded, since it requires reusing the 'a'
+        self.assertEquals(set(['add']), words)
+
 if __name__ == '__main__':
     unittest.main()
 
